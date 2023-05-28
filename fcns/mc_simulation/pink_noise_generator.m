73718a287649968b9c12e9583562a919520e942f
jagatpreet <nir.j@northeastern.edu>
2023-05-28 19:50:29 -0400

    refactored pink noise function

--- fcns/mc_simulation/pink_noise_generator.m
+++ fcns/mc_simulation/pink_noise_generator.m
@@ -1,30 +1,30 @@
-function scaled_pink = pink_noise_gen1(sigma,tau1,tau2, M, del_t)
-%     pn = dsp.ColoredNoise('pink',num_samples,3);
-%     unscaled_pink = pn();
-%     scaled_pink = BiasInstabilityTerm.*unscaled_pink;
-    %del_t = 1/400;
+function pink_noise = pink_noise_generator(sigma,tau1,tau2, M, del_t)
+%   pink_noise_generator creates a noise from summation of two 
+% AR(1) processes. 
+
+    % Preallocate
     np1 = randn(M,1);
     np2 = randn(M,1);
-         
+    
+    % define sigma for t1 and t2
     sigma1 = sigma;
-      
     sigma2 = sigma;
       
-    T_c1 = tau1/1.89
-    q_c1 = sigma1/0.437/sqrt(T_c1)
+    T_c1 = tau1/1.89;
+    q_c1 = sigma1/0.437/sqrt(T_c1);
 
-    T_c2 = tau2/1.89
-    q_c2 = sigma2/0.437/sqrt(T_c2)
+    T_c2 = tau2/1.89;
+    q_c2 = sigma2/0.437/sqrt(T_c2);
 
     % Initial bias
-    b_0.acc = 0;%imu_params.Accelerometer.b_on;
-    b_0.gyro = 0;%imu_params.Gyroscope.b_on;
-
+    b_0.acc = 0;
+    b_0.gyro = 0;
+    
     % Preallocate
     bp1 = zeros(M,1);
     bp2 = zeros(M,1);
 
-    % Bias model with brown and pink noise
+    % Bias model with pink noise
     for i = 1:M
         if i == 1
             bp1(i) = b_0.acc;
@@ -35,5 +35,5 @@ function scaled_pink = pink_noise_gen1(sigma,tau1,tau2, M, del_t)
         end
     end
 
-    scaled_pink = bp1 + bp2;
+    pink_noise = bp1 + bp2;
 end 
