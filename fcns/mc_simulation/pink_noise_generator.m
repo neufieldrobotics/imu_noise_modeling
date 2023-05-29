7562acfb0da9ea457c1be9bf7172062255bdc1c1
jagatpreet <nir.j@northeastern.edu>
2023-05-28 20:20:34 -0400

    refactored to use new function for ar1 process

--- fcns/mc_simulation/pink_noise_generator.m
+++ fcns/mc_simulation/pink_noise_generator.m
@@ -9,31 +9,8 @@ function pink_noise = pink_noise_generator(sigma,tau1,tau2, M, del_t)
     % define sigma for t1 and t2
     sigma1 = sigma;
     sigma2 = sigma;
-      
-    T_c1 = tau1/1.89;
-    q_c1 = sigma1/0.437/sqrt(T_c1);
-
-    T_c2 = tau2/1.89;
-    q_c2 = sigma2/0.437/sqrt(T_c2);
-
-    % Initial bias
-    b_0.acc = 0;
-    b_0.gyro = 0;
-    
-    % Preallocate
-    bp1 = zeros(M,1);
-    bp2 = zeros(M,1);
-
-    % Bias model with pink noise
-    for i = 1:M
-        if i == 1
-            bp1(i) = b_0.acc;
-            bp2(i) = b_0.acc;
-        else
-            bp1(i) = bp1(i-1) - del_t/T_c1*bp1(i-1) + del_t*q_c1*np1(i);
-            bp2(i) = bp2(i-1) - del_t/T_c2*bp2(i-1) + del_t*q_c2*np2(i);
-        end
-    end
-
+    sampling_rate = 1/del_t;
+    bp1 = simulate_AR1_process(M, sampling_rate, sigma1, tau1);
+    bp2 = simulate_AR1_process(M, sampling_rate, sigma2, tau2);
     pink_noise = bp1 + bp2;
 end 
