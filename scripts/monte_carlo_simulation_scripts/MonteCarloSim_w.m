3685e022153bed878dca474fd6ecaa48855df830
jagatpreet <nir.j@northeastern.edu>
2023-05-29 13:45:10 -0400

    refactored to make general code

--- scripts/monte_carlo_simulation_scripts/MonteCarloSim_w.m
+++ scripts/monte_carlo_simulation_scripts/MonteCarloSim_w.m
@@ -5,11 +5,11 @@ config_MC_sim_w;
 for run=1:total_runs
     disp(' Accelerometer Run number = ');
     disp(run);
-    mc_sim_run_data = dead_reckoning_noise_model(VN100_params{1},sim_params);
+    mc_sim_run_data = dead_reckoning_noise_model(imu_params{1},sim_params);
     mc_sim_run_data.doc.comment = strcat("Accelerometer - ",...
                             "white noise simulation"," Run - ",num2str(run));
     mc_sim_run_data.doc.order = "x y z";
-    mc_sim_run_data.doc.noise_params = VN100_params{1};
+    mc_sim_run_data.doc.noise_params = imu_params{1};
     append_header_to_MC_data;
     save(strcat(filepath.save_path.acc,'_',num2str(run),'.mat'),'mc_sim_run_data');
 end
@@ -18,11 +18,11 @@ end
 for run=1:total_runs
     disp(' Gyroscope Run number = ');
     disp(run);
-    mc_sim_run_data = dead_reckoning_noise_model(VN100_params{2},sim_params);
+    mc_sim_run_data = dead_reckoning_noise_model(imu_params{2},sim_params);
     mc_sim_run_data.doc.comment = strcat("Gyroscope - ",...
                         "white noise simulation"," Run - ",num2str(run));
     mc_sim_run_data.doc.order = "x y z";
-    mc_sim_run_data.doc.noise_params = VN100_params{2};
+    mc_sim_run_data.doc.noise_params = imu_params{2};
     append_header_to_MC_data;
     save(strcat(filepath.save_path.gyro,'_',num2str(run),'.mat'),'mc_sim_run_data');
 end
